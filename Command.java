142659c64125051789a28c289fc200681d4368e2
ianm24 <ianmcdowell24@gmail.com>
2020-03-23 19:34:21 -0400

    Made Command Interface

---
+++ Command.java
@@ -0,0 +1,12 @@
+/**
+ * The Command interface is used for the various
+ * commands within this project
+ * @author Ian McDowell 2020
+ *
+ */
+
+public interface Command {
+
+	public void execute();
+	
+}

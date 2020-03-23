dc9e33ad6afdf766f18f026ecd9d7f36c63fa8c1
Tyron Schultz <tyronsonfire@gmail.com>
2020-03-23 19:59:43 -0400

    fixed InputHandler

--- inputHandler.java
+++ inputHandler.java
@@ -12,6 +12,10 @@ public class InputHandler {
   private HashMap<String, Command> map = new HashMap();
 
   public InputHandler(Document doc) {
+	Command LoadCommand = new LoadCommand;
+	Command SaveCommand = new SaveCommand;
+	Command Spell = new LoadCommand;
+	Command LoadCommand = new LoadCommand;
     map.put("load", LoadCommand);
     map.put("save", SaveCommand(doc));
     map.put("spell", SpellCheckCommand(doc));

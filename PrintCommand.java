3bc4572d2422326727c16c6b7d13be48d51c153e
rdsoard <rsoard@LAPTOP-PHQO3L0D.localdomain>
2020-03-23 20:23:42 -0400

    SpellCommand and PrintCommand with javadoc

--- PrintCommand.java
+++ PrintCommand.java
@@ -1,15 +1,21 @@
 package CommandDesignPattern;
 /**
- * 
+ * This class is for the print command
  * @author Bekah
  *
  */
 public class PrintCommand implements Command {
 	private Document doc;
+	/**
+	 *  This method is a constructor
+	 * @param doc
+	 */
     public PrintCommand(Document doc) {
 		this.doc = doc;
 	}
-	
+	/**
+	 * This method executes the spell command
+	 */
 	public void execute() {
 		doc.print();
 	}
--- SpellCommand.java
+++ SpellCommand.java
@@ -1,15 +1,21 @@
 package CommandDesignPattern;
 /**
- * 
+ * This class is for the spell command
  * @author Bekah
  *
  */
 public class SpellCommand implements Command {
 	private Document doc;
+	/**
+	 * This method is a constructor 
+	 * @param doc
+	 */
 	public SpellCommand(Document doc) {
 		this.doc = doc;
 	}
-	
+	/**
+	 * This method executes the spell command
+	 */
 	public void execute() {
 		doc.spell();
 	}

52b1fadff664a7ac38e7aa8a655e84612a26347d
milz L <13milesj@gmail.com>
2020-03-23 20:09:04 -0400

    finishing up save command

--- SaveCommand.java
+++ SaveCommand.java
@@ -1,5 +1,29 @@
 package CommandDesignPattern;
+/**
+ * 
+ * @author Miles
+ * This class handles saving the command
+ */
 
-public class SaveCommand implements Command{
 
+public class SaveCommand implements Command {
+  private Document doc;
+  
+  /**
+   * Creates a document object
+   * @param doc
+   * 
+   */
+  public SaveCommand(Document doc) {
+    this.doc = doc;
+  }
+  
+  
+  /**
+   * Executes the save command
+   */
+  public void execute() {
+    doc.save();
+  }
+  
 }
